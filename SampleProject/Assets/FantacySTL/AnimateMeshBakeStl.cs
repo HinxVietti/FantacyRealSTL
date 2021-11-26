1c21e5e664335f812f1d7e58f80b2b49228bc0cc
Hinx Vietti <hinxvietti@gmail.com>
2021-11-26 17:21:45 +0800

    add codes and demos

---
+++ SampleProject/Assets/FantacySTL/AnimateMeshBakeStl.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateMeshBakeStl : MonoBehaviour
+{
+    [SerializeField] SkinnedMeshRenderer[] targets;
+#if UNITY_EDITOR
+
+    private void Reset()
+    {
+        targets = GetComponentsInChildren<SkinnedMeshRenderer>();
+    }
+
+#endif
+
+    private Mesh[][] takeAnSnap()
+    {
+        int size = targets.Length;
+        List<MeshFilter> filters = new List<MeshFilter>();
+        Matrix4x4[] localMatrixs = new Matrix4x4[size];
+        CombineInstance[] combine = new CombineInstance[size];
+        List<Mesh> singleMeshes = new List<Mesh>();
+
+        int i = 0;
+        for (; i < size; i++)
+        {
+            Mesh target = new Mesh();
+            var rdn = targets[i];
+            localMatrixs[i] = rdn.transform.localToWorldMatrix;
+            rdn.BakeMesh(target);
+            target.name = $"bk_{i }_{rdn.sharedMesh.name}";
+            combine[i] = new CombineInstance { transform = localMatrixs[i], mesh = target };
+            singleMeshes.Add(target);
+        }
+
+        Mesh combinedMesh = new Mesh();
+        combinedMesh.CombineMeshes(combine);
+
+        return new Mesh[][] {
+            new []{ combinedMesh},
+           singleMeshes.ToArray()
+        };
+    }
+
+
+    public Mesh[][] TakeSnapshot()
+    {
+        return takeAnSnap();
+    }
+
+}
---
+++ SampleProject/Assets/FantacySTL/Mesh2STLSaver.cs
@@ -0,0 +1,131 @@
+﻿/*******************************
+	Author : Mobiano.Hinx
+	Email: laizhixin@wonderidea.com
+********************************/
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+[RequireComponent(typeof(AnimateMeshBakeStl))]
+public class Mesh2STLSaver : MonoBehaviour
+{
+    private bool reverseAxis = true;
+    private bool CreateMeshesOnScene = false;
+
+    private void OnGUI()
+    {
+        GUILayout.Label("This is an 'Editor Only Scripts' create by Hinx.");
+        CreateMeshesOnScene = GUILayout.Toggle(CreateMeshesOnScene, "Create Meshes On Scene when shot.");
+        reverseAxis = GUILayout.Toggle(reverseAxis, "Reverse Axis for Unity");
+        if (GUILayout.Button($"take snap and save '{name}'"))
+        {
+            string outputFolder = UnityEditor.EditorUtility.SaveFolderPanel("select an save location", "", name);
+            if (string.IsNullOrEmpty(outputFolder))
+                return;
+            var tool = GetComponent<AnimateMeshBakeStl>();
+            var items = tool.TakeSnapshot();
+            var singleMeshes = items[1];
+            int singleMeshesCount = singleMeshes.Length;
+            float total = singleMeshesCount + 1;
+            string title = "converting..";
+            EditorUtility.DisplayProgressBar(title, "begin convert combined mesh", 0);
+            if (CreateMeshesOnScene)
+                CreateMeshes(combined: items[0][0], separate: items[1]);
+            string combinedSaveName = Path.Combine(outputFolder, $"combined_{name}.stl");
+            saveMeshToStl(items[0][0], combinedSaveName);
+            EditorUtility.DisplayProgressBar(title, "begin convert separated mesh", 1f / total);
+            int index = 0;
+            for (int i = 0; i < singleMeshesCount; i++)
+            {
+                var mesh = singleMeshes[i];
+                string saveName = Path.Combine(outputFolder, $"separate_{mesh.name}.stl");
+                saveMeshToStl(mesh, saveName);
+                index++;
+                EditorUtility.DisplayProgressBar(title, "begin convert separated mesh", (index + 1) / total);
+            }
+
+            //finshed;
+            EditorUtility.ClearProgressBar();
+            Application.OpenURL(outputFolder);
+            Debug.Log("FINISHED.");
+        }
+    }
+
+    /// <summary>
+    /// Convert and save stl
+    /// </summary>
+    /// <param name="mesh"></param>
+    /// <param name="location"></param>
+    private void saveMeshToStl(Mesh mesh, string location)
+    {
+
+        //NEEDS TO OPTIMIZE IF YOU HAVE A LOT OF VERTICES
+        STL stl = STL.CreateEmpty();
+        stl.header = new STL_Header($"_hinx_stl_unity_{mesh.name}");
+
+        int meshVerticesCount = mesh.vertices.Length;
+        var triangles = mesh.triangles;
+        int length3 = triangles.Length;
+        if (length3 % 3 != 0)
+            throw new Exception("error");
+        int triangleCount = length3 / 3;
+        stl.triangles = new STL_Triangle[triangleCount];
+        Func<Vector3, float[]> convert = v =>
+        {
+            if (reverseAxis)
+                return new[] { v.x, v.z, v.y };//x z y
+            return new[] { v.x, v.y, v.z };//x y z
+        };
+        for (int i = 0; i < triangleCount; i++)
+        {
+            int p01 = triangles[i * 3];
+            int p02 = triangles[i * 3 + 1];
+            int p03 = triangles[i * 3 + 2];
+            stl.triangles[i] = new STL_Triangle
+            {
+                AttrData = 0,
+                Normal = convert(mesh.normals[p01]),//
+                Vertex1 = convert(mesh.vertices[p01]),
+                Vertex2 = convert(mesh.vertices[p02]),
+                Vertex3 = convert(mesh.vertices[p03]),
+            };
+        }
+        stl.TriangleCount = stl.triangles.Length;
+        if (File.Exists(location))
+            File.Delete(location);
+        using (var fs = File.Create(location))
+        {
+            stl.SaveToBinary(fs);
+            fs.Flush();
+        }
+    }
+
+
+    private void CreateMeshes(Mesh combined, Mesh[] separate)
+    {
+        var obj1 = createMeshObj(combined);
+        combined.RecalculateBounds();
+        obj1.name = "combined";
+        obj1.transform.position = Vector3.right * combined.bounds.size.x;
+
+        var obj2 = new GameObject("separate");
+        for (int i = 0; i < separate.Length; i++)
+        {
+            var child = createMeshObj(separate[i]);
+            child.SetParent(obj2.transform);
+        }
+    }
+
+
+    private Transform createMeshObj(Mesh mesh)
+    {
+        var go = new GameObject(mesh.name);
+        go.AddComponent<MeshFilter>().mesh = mesh;
+        go.AddComponent<MeshRenderer>().material = new Material(Shader.Find("Standard"));
+        return go.transform;
+    }
+}
